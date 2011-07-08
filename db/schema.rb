f206628e8786ce20bf4212efddc13d13811d173d
David <none@none>
2011-07-08 17:15:23 +0200

    deployment tweaks

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110315124941) do
+ActiveRecord::Schema.define(:version => 20110218163214) do
 
   create_table "activity_logs", :force => true do |t|
     t.string   "action"
