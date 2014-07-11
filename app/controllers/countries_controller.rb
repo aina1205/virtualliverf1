8ffd072df7563c552d70a32c02b9b22d24459b16
lihuaan@localhost6.localdomain6 <lihuaan@localhost6.localdomain6>
2014-07-11 14:00:25 +0200

    local fix 1 : only logged in user can view institutions and people

--- app/controllers/countries_controller.rb
+++ app/controllers/countries_controller.rb
@@ -9,7 +9,17 @@ class CountriesController < ApplicationController
     @institutions = Institution.find(:all, :conditions => ["country LIKE ?", @country])
     
     respond_to do |format|
-      format.html # show.html.erb
+      if Seek::Config.is_virtualliver
+        if !User.current_user.nil?
+          format.html # show.html.erb
+        else
+          store_location
+          flash[:error] = "You are not authorized to view institutions and people in this country, you may need to login first."
+          format.html { redirect_to root_url}
+        end
+      else
+        format.html # show.html.erb
+      end	     
     end
   end
 end
